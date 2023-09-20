0d4cb1f2bce21d7c2ad37bb93d0e5b0463d028ee
MaximePeterburg <getmax4444@gmail.com>
2023-09-20 19:18:23 +0300

    added rtk query auth api

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-
+import { apiSlice } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer
   },
-  
-  
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true
 });
